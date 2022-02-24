1350ee0b625c71028b1417c62922f75b4c1eb7cd
Jamal <jamalpeaksoft@gmail.com>
2022-02-24 20:55:37 +0600

    ozgoryldy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,13 @@ public class Main {
     public static void main(String[] args) {
 //   Class деген Ссылка  менен Обьект  тузуш  керек
 	Class cancoz = new Class(69,"Жамал эже", new int[]{1, 9, 5});
-        System.out.println(" Массивдин элементтери ^");
+        System.out.println(" nomer деген  поледеги сан - "+ cancoz.getNomer()+"; соз деген поледеги  соз - "+cancoz.getCoz()+";");
+       System.out.println(" Массивдин элементтери ^");
         // forarech  массивдин  элементтерин  консолго  чыгаруучу цикл
         for (int p:new int[]{1,9,5}) {
-            System.out.println(p);
+            System.out.print(" "+p+" ,");
 
 
         }
-        }
-}
+        }}
+
